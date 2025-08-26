be052254e9ebe379fd7d329c77907d5da21a9901
choyounho-lab <choyh8@naver.com>
2025-08-26 17:44:43 +0900

    기백아2
    2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -179,7 +179,7 @@ const Home: React.FC = () => {
               <Meta>{exhibit.period ?? ""}</Meta>
             </InfoCard>
           )} */}
-
+                    <div>aaa3213124124aaa</div>
                     <FabMenu title="메뉴">≡</FabMenu>
                     <div>중근티비치킨</div>
                 </Content>
