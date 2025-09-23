6a3ef21e1ab77c3d7ae58dd2a0242f4d7dee0b3d
choyounho-lab <choyh8@naver.com>
2025-09-23 18:08:37 +0900

    조윤호오
    라우터 변경

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,38 +1,37 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Layout from "../layout/Layout";
-import Home from "../pages/Home";
-import Admin from "../pages/Admin";
-import Login from "../member/Login";
-import Collection from "../pages/Collection";
-import RelicDetailPage from "../pages/RelicDetailPage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import Home from '../pages/Home';
+import Admin from '../pages/Admin';
+import Login from '../member/Login';
+import Collection from '../pages/Collection';
+import RelicDetailPage from '../pages/RelicDetailPage';
 
-import ExhibitionPage from "../pages/ExhibitionPage";
+import ExhibitionPage from '../pages/ExhibitionPage';
 
 function Router() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* 로그인 */}
-          <Route path="member/login" element={<Login />} />
-          <Route path="collection" element={<Collection />} />
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
 
-        {/* 로그인 */}
-        
-      </Route>
-<Route path="member/login" element={<Login />} />
-      <Route path="collection" element={<Collection />} />
-      <Route path="/detail/:id" element={<RelicDetailPage />} />
-      {/* 어드민 */}
-        <Route path="admin" element={<Admin />} />
-    </Routes>
-          {/* 전시 페이지 */}
-          <Route path="exhibition/:type" element={<ExhibitionPage />} />
-        </Route>
-      </Routes>
-    </>
-  );
+                    {/* 로그인 */}
+
+                    <Route path="member/login" element={<Login />} />
+                    <Route path="collection" element={<Collection />} />
+                    <Route path="/detail/:id" element={<RelicDetailPage />} />
+                    {/* 어드민 */}
+                    <Route path="admin" element={<Admin />} />
+
+                    {/* 전시 페이지 */}
+                    <Route
+                        path="exhibition/:type"
+                        element={<ExhibitionPage />}
+                    />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 export default Router;
